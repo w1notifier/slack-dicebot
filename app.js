f70afb9708d06b030204173de6f29f05850fa268
Pete Lumbis <plumbis@cumulusnetworks.com>
2015-11-01 13:45:47 -0500

    Removed hellobot

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var hellobot = require('./hellobot');
 var dicebot = require('./dicebot');
 
 var app = express();
@@ -12,9 +11,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // test route
 app.get('/', function (req, res) { res.status(200).send('Hello world!') });
-
-// hellobot
-app.post('/hello', hellobot);
 
 // dicebot
 app.post('/roll', dicebot);
