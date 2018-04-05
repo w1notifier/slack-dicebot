36fb0c62ad6b1f0b1b3b2671850ed59adabf03b0
mack <uds.mack@gmail.com>
2018-04-05 13:32:59 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var dicebot = require('./dicebot');
 var who = require('./whobot');
 
 var app = express();
@@ -9,21 +8,17 @@ var port = process.env.PORT || 3000;
 // body parser middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
 // test route
 app.get('/', function (req, res) { res.status(200).send('Go away') });
 
 // dicebot
-app.post('/roll', dicebot);
 app.post('/who', who);
-
 
 // basic error handler
 app.use(function (err, req, res, next) {
   console.error(err.stack);
   res.status(400).send(err.message);
 });
-
 
 app.listen(port, function () {
   console.log('Slack bot listening on port ' + port);
